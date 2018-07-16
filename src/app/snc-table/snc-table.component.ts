31129fafe0204faa6c137476ed5ecc687d232274
Izabela <cizabelacristina@gmail.com>
2018-07-16 16:45:09 -0300

    [FIX] Tornando linhas escondidas na ordenação

--- src/app/snc-table/snc-table.component.ts
+++ src/app/snc-table/snc-table.component.ts
@@ -22,9 +22,9 @@ import 'rxjs/add/observable/of';
   styleUrls: ['./snc-table.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('collapsed, void', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
       state('expanded', style({ height: '*', visibility: 'visible' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('* <=> *', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
@@ -111,4 +111,9 @@ export class SncTableComponent implements OnInit, OnDestroy {
 
       return nomes.slice(1).join(' ');
   }
+
+  clearExpandedElements($event) {
+    this.expandedElement = false;
+  }
+
 }

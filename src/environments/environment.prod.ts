baa0e8d54607465630f272ecf9dbc92dfd0a0cc6
Izabela Cardoso <izabela.cardoso@lablivre.wiki.br>
2019-01-29 17:16:02 -0200

    Atualiza link da api em produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://snc.cultura.gov.br/api/v1/sistemadeculturalocal/'
+  API_URL: 'http://snc.cultura.gov.br/api/v2/sistemadeculturalocal/'
 };

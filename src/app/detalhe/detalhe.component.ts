7423872694dc95c4d84745cd19f2da6ea88df40a
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-03-26 13:50:12 -0300

    [ADD] adicionando componente para exibir os detalhes do Ente

---
+++ src/app/detalhe/detalhe.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, NavigationStart, Router} from "@angular/router";
+import {EnteService}from '../ente.service';
+import {Observable} from "rxjs";
+import {filter, map} from "rxjs/operators";
+
+@Component({
+  selector: 'snc-detalhe',
+  templateUrl: './detalhe.component.html',
+  styleUrls: ['./detalhe.component.css'],
+})
+export class DetalheComponent implements OnInit {
+  private teste = "oi";
+  private id = 0;
+  private entidade = {};
+  constructor(private ente: EnteService) { }
+
+  ngOnInit() {
+
+
+    this.entidade = this.ente.getEnte();
+
+  }
+
+}

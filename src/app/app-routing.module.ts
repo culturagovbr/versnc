4d43b12ef0f69f6875d35aba93a05ec309ef5420
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-03-26 13:51:28 -0300

    [UPDATE] atualizando lista de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {SncTableComponent} from './snc-table/snc-table.component';
+import {DetalheComponent} from "./detalhe/detalhe.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'tabela-uf-municipio', component: SncTableComponent },
+  { path: 'detalhe', component: DetalheComponent },
 ];
 
 @NgModule({

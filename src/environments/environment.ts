11b067d439a84d14b02c85ed34dda063f95c4d58
Ronald P Jr <ronald.junior@turismo.gov.br>
2020-08-17 16:57:08 -0300

    Alteraçao apontamento API homologaçao

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   //API_URL: 'http://127.0.0.1:8000/api/v2/sistemadeculturalocal/'
-  //API_URL: 'http://hmg.snc.cultura.gov.br/api/v2/sistemadeculturalocal/'
-  API_URL: 'http://localhost:8000/api/v2/sistemadeculturalocal/'
+  API_URL: 'http://hmg.snc.cultura.gov.br/api/v2/sistemadeculturalocal/'
+  //API_URL: 'http://localhost:8000/api/v2/sistemadeculturalocal/'
 };
